b22eab6dd694cf72b0713b1fb37d9f0d26c226f2
Kush <157900212+kushrevankar@users.noreply.github.com>
2024-11-16 12:12:47 +0530

    Update CardList.jsx

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -6,7 +6,7 @@ import Pagination from '../pagination/Pagination';
 import Card from '../card/Card';
 
 const getData = async (page,cat) => {
-  const res = await fetch(`/api/posts?page=${page}&cat=${cat || ""}`, {
+  const res = await fetch(`https://fsdblogsite.vercel.app/api/posts?page=${page}&cat=${cat || ""}`, {
     cache: "no-store",
   });
 
